3d6a5676162dd1e0d5d53f8d705e0c24b591e586
Abdullah Ajayi <hafizajayi2308@gmail.com>
2024-01-23 15:11:36 +0100

    Added data folder

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import image_1 from "../assets/images/image_1.jpg";
+import image_2 from "../assets/images/image_2.jpg";
+import image_3 from "../assets/images/image_3.jpg";
+import image_4 from "../assets/images/image_4.jpg";
+import image_5 from "../assets/images/image_5.jpg";
+
+export const sections = [
+  {
+    image: image_1,
+    header: "Front of Codel",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+      sapiente at voluptatem rem nobis debitis porro! Voluptatem
+      aperiam aspernatur dolor.`,
+  },
+  {
+    image: image_2,
+    header: "Front of Codel",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+      sapiente at voluptatem rem nobis debitis porro! Voluptatem
+      aperiam aspernatur dolor.`,
+  },
+  {
+    image: image_2,
+    header: "Front of Codel",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+      sapiente at voluptatem rem nobis debitis porro! Voluptatem
+      aperiam aspernatur dolor.`,
+  },
+];
+
+export const JetSvgs = [
+  { scale: 1, top: 10, left: 20 },
+  { scale: 1, top: 20, left: 30 },
+  { scale: 1.1, top: 15, left: 40 },
+];
