7a077c8582dcab9d648816d7247e3bbaa226f799
Abdullah Ajayi <hafizajayi2308@gmail.com>
2024-01-24 15:12:05 +0100

    Added distance to jet JetSvgs array element

--- src/data/data.js
+++ src/data/data.js
@@ -3,25 +3,34 @@ import image_2 from "../assets/images/image_2.jpg";
 import image_3 from "../assets/images/image_3.jpg";
 import image_4 from "../assets/images/image_4.jpg";
 import image_5 from "../assets/images/image_5.jpg";
+import image_8 from "../assets/images/image_8.jpg";
+import image_14 from "../assets/images/image_14.jpg";
 
 export const sections = [
   {
     image: image_1,
-    header: "Front of Codel",
+    header: "getting drone ready for flight",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
       sapiente at voluptatem rem nobis debitis porro! Voluptatem
       aperiam aspernatur dolor.`,
   },
   {
     image: image_2,
-    header: "Front of Codel",
+    header: "drone pilot",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
       sapiente at voluptatem rem nobis debitis porro! Voluptatem
       aperiam aspernatur dolor.`,
   },
   {
-    image: image_2,
-    header: "Front of Codel",
+    image: image_3,
+    header: "final preparation",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+      sapiente at voluptatem rem nobis debitis porro! Voluptatem
+      aperiam aspernatur dolor.`,
+  },
+  {
+    image: image_8,
+    header: "nice baby",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
       sapiente at voluptatem rem nobis debitis porro! Voluptatem
       aperiam aspernatur dolor.`,
@@ -29,7 +38,7 @@ export const sections = [
 ];
 
 export const JetSvgs = [
-  { scale: 1, top: 10, left: 20 },
-  { scale: 1, top: 20, left: 30 },
-  { scale: 1.1, top: 15, left: 40 },
+  { scale: 1, top: 10, left: 20, distance: 450 },
+  { scale: 1, top: 20, left: 30, distance: 300 },
+  { scale: 1.1, top: 15, left: 40, distance: 650 },
 ];
